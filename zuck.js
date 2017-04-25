104b3bf3567c6f744ac4aa0d0b8145af40744b48
egfx <egfx@users.noreply.github.com>
2017-04-24 21:28:50 -0700

    Update zuck.js

--- zuck.js
+++ zuck.js
@@ -21,9 +21,6 @@ window['ZuckitaDaGalera'] = window['Zuck'] = function(timeline, options) {
                 return '';
             }
         },
-	h = function (value){
-  		return $('<div/>').html(value).text();
-	},
         each = function(arr, func) {
             if (arr) {
                 var total = arr.length;
@@ -401,8 +398,9 @@ window['ZuckitaDaGalera'] = window['Zuck'] = function(timeline, options) {
 				pointerItems += '<span '+commonAttrs+' class="' + ((currentItem === i) ? 'active' : '') + ' '+seemClass+'"><b style="animation-duration:' + ((length === '') ? '3' : length) + 's"></b></span>';
 				htmlItems += '<div data-time="'+g(item, 'time')+'" data-type="' + g(item, 'type') + '"'+commonAttrs+' class="item ' + seemClass +
 					' ' + ((currentItem === i) ? 'active' : '') + '">' +	
-					((g(item, 'type') === 'app') ? h(g(item, 'src')) : '') +
-					((g(item, 'type') === 'video') ? '<video class="media" muted webkit-playsinline playsinline preload="auto" src="' + g(item, 'src') + '" ' + g(item, 'type') + '></video><b class="tip muted">' + option('language', 'unmute') + '</b>' : '<img class="media" src="' + g(item, 'src') + '" ' + g(item, 'type') + '>') +
+					((g(item, 'type') === 'app') ? g(item, 'src') : '') +
+					((g(item, 'type') === 'photo') ? g(item, 'src') : '<img class="media" src="' + g(item, 'src') + '" ' + g(item, 'type') + '>') +
+					((g(item, 'type') === 'video') ? '<video class="media" muted webkit-playsinline playsinline preload="auto" src="' + g(item, 'src') + '" ' + g(item, 'type') + '></video><b class="tip muted">' + option('language', 'unmute') + '</b>' : '') +
 					((g(item, 'link')) ? '<a class="tip link" href="'+g(item, 'link')+'" rel="noopener" target="_blank">' + ((linkText == '') ? option('language', 'visitLink') : linkText) + '</a>' : '') +
 					'</div>';
 			});
